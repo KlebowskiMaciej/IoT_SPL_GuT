d4f3471eba8a021a4362737024fa3cca5ad48228
Maciej Kłębowski <82090956+KlebowskiMaciej@users.noreply.github.com>
2023-01-02 23:57:05 +0100

    Update Audio.py
    exception handling, minor corrections

--- Audio.py
+++ Audio.py
@@ -8,65 +8,51 @@ import threading
 
 class Recorder(object):
 
-    # Data pack locally
-    global local_data
-
     local_data = []
-
-    #Data pack Globally
-    global global_data
-
     global_data = []
 
     CHUNK = 1024  # frames to keep in buffer between reads
     SAMP_RATE = 44100  # sample rate [Hz]
     FORMAT = pyaudio.paInt16  # 16-bit device
-    # BUFFOR_FORMAT  = np.int16 # 16-bit for buffer
     CHANNEL = 1  # only read 1 channel mono 2 stereo
     DEV_INDEX = 0  # index of sound device
-    PAUSE = False
+    PAUSE = False # variable that stops the application running
     FRAME_COUNT = 0
-    SECONDS = 30
-    SIZE_SEND_DATA = 10
+    SECONDS = 30  # number of seconds from which one maximum is taken
+    SIZE_SEND_DATA = 10 #  total number of max to compare 
 
     def __init__(self):
+        self.process()
 
-        self.analyzer()
-
-   
-
-    def analyzer(self):
-        self.set()
+    def process(self):
+        
+        self.set_mic()
         error_count = 0
         start_time = time.time()
-
         print('stream started')
-
         while not self.PAUSE:
             try:
-                stream_data = self.stream.read(self.CHUNK, exception_on_overflow=False)  # grab data frames from buffer
+                stream_data = self.stream.read(self.CHUNK, exception_on_overflow=False)  
                 current_time = time.time()
             except IOError as e:
                 error_count += 1
-                print("(%d) Error %s" & (error_count, e))
+                print("({}) Error {}".format(error_count, e))
             else:
-                elapsed_time = current_time - start_time
-                local_data.append(self.process(stream_data))
-                if elapsed_time > self.SECONDS:
+                elapsed_time = current_time - start_time  #
+                self.local_data.append(self.to_dB(stream_data))
+                if elapsed_time >= self.SECONDS:
                     start_time = time.time()
-                    self.send()
-                    local_data.clear()
-
-
+                    self.send_data()
+                    self.local_data.clear()
         else:
             fr = self.FRAME_COUNT / (time.time() - start_time)
-            print('average frame rate = {:.0f} FPS'.format(fr))
+            print("average frame rate = {:.0f} FPS".format(fr))
 
-    def process(self, data_vec):
+    def to_dB(self, data_vec):
         reading = audioop.max(data_vec, 2)
         return 20 * (np.log10(abs(reading)))
 
-    def set(self):
+    def set_mic(self):
         self.audio = pyaudio.PyAudio()
         self.stream = self.audio.open(format=self.FORMAT,
                                       channels=self.CHANNEL,
@@ -80,28 +66,30 @@ class Recorder(object):
         self.stream.close()
         self.audio.terminate()
 
+    def send_data(self):
+        value = max(self.local_data)
+        self.global_data.append(value)
 
-    def send(self):
-        value = max(local_data)
-        global_data.append(value)
-
-        if len(global_data)>=10:
-           now = datetime.datetime.now()
+        if len(self.global_data)>=10:
+            now = datetime.datetime.now()
             google = GoogleSheets.GoogleWrite()
-            time = ""+str(now.hour)+":"+str(now.minute)+":"+str(now.second)
-            value = max(global_data)
+            var_time = ""+str(now.hour)+":"+str(now.minute)+":"+str(now.second) # stacking the hour to a variable
+            var_data = max(self.global_data)
+            self.global_data.clear()
             
-            global_data.clear()
+            try:
+                task_google = threading.Thread(target=google.send,args=(var_data,var_time))
+                task_azure = threading.Thread(target=print,args=("Azure send"))
+                
+                task_azure.start()    
+                task_google.start()
             
-            task_google = threading.Thread(target=google.send,args=(value,time))
-            task_azure = threading.Thread(target=print,args=("Azure send"))
-            #Send To Azure
-            task_azure.start()
+            except Exception as e:
+                print(e)  # writing out a exception message 
+                pass
             
-            #Send To Google Sheets            
-            task_google.start()
-
-
+            finally:
+                print("\nValue in dB: "+str(var_data)+" Time: "+str(var_time))
 
 if __name__ == '__main__':
     Recorder()
